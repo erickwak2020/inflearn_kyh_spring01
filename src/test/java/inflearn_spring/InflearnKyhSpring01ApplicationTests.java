b6981be97eb273be09626529ab00caef1a1dd5e0
곽정환 <erickwak2020@gmail.com>
2022-01-04 20:54:37 +0900

    Initial commit

---
+++ src/test/java/inflearn_spring/InflearnKyhSpring01ApplicationTests.java
@@ -0,0 +1,13 @@
+package inflearn_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnKyhSpring01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
